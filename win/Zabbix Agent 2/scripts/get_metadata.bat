3642c3c8c55dfada868c727f0fcd19ee6abd3390
tech6 <aadrujinin@gmail.com>
2026-07-10 21:15:28 +0300

    asd

--- win/Zabbix Agent 2/scripts/get_metadata.bat
+++ win/Zabbix Agent 2/scripts/get_metadata.bat
@@ -1,9 +1,2 @@
 @echo off
-setlocal enabledelayedexpansion
-set "CHARS=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-set "RANDSTR="
-for /L %%i in (1,1,8) do (
-    set /a "idx=!random! %% 62"
-    for %%j in (!idx!) do set "RANDSTR=!RANDSTR!!CHARS:~%%j,1!"
-)
-echo windows_!RANDSTR!
+echo Windows
